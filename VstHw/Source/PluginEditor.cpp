6724e9345d6e2cada328e2d6ec8fa0e8df731f4e
ruslan_g <xsabitov@gmail.com>
2020-10-04 17:55:05 +0300

    Editor and processor

---
+++ VstHw/Source/PluginEditor.cpp
@@ -0,0 +1,55 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+NewProjectAudioProcessorEditor::NewProjectAudioProcessorEditor (NewProjectAudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    m_gainSlider.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
+    m_gainSlider.setTextBoxStyle(juce::Slider::TextBoxBelow, true, 50, 20);
+    m_gainSlider.setRange(-60.0f, 0.0f, 0.01f);
+    m_gainSlider.setValue(-20.f);
+    m_gainSlider.addListener(this);
+    addAndMakeVisible(m_gainSlider);
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (400, 300);
+}
+
+NewProjectAudioProcessorEditor::~NewProjectAudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void NewProjectAudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    //g.setColour (juce::Colours::white);
+    //g.setFont (15.0f);
+    //g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void NewProjectAudioProcessorEditor::resized()
+{
+    m_gainSlider.setBounds((getWidth() >> 1) - 50, getHeight() >> 1, 100, 150);
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+}
+
+void NewProjectAudioProcessorEditor::sliderValueChanged(juce::Slider* slider)
+{
+    if (slider == &m_gainSlider)
+    {
+        static_cast<NewProjectAudioProcessor*>(&processor)->setGain(slider->getValue());
+    }
+}
